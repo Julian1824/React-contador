e387f5b870287df86f084c0c2a1ea3b465315c66
Julian1824 <julianbs1813@gmail.com>
2022-08-01 11:11:11 -0500

    Proyecto React realiza la gestion y conteo dinamicamente

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import Boton from './compometes/Boton.js';
+import Contador from './compometes/Contador.js';
+import logo from './img/pngegg.png';
+import {useState} from 'react';
 
 function App() {
+
+  const [numClics, setNumClics] =  useState(0);
+
+  const manejarClic = () => {
+      setNumClics(numClics+100000000);
+  }
+  const reiniciarClic = () => {
+    setNumClics(0);
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='logo-feedend-contenedor'>
+          <img
+            className='logo-feenend'
+            src={logo}
+            alt='Logo feenend'
+            />
+        </div>
+      <div className='contenedor-principal'>
+        
+        <Contador 
+        numClics={numClics}
+        />
+        <Boton 
+        texto='Clic'
+        esBotonClic={true}
+        manejarClic={manejarClic}
+        />
+        <Boton 
+        texto='Reiniciar'
+        esBotonClic={false}
+        manejarClic={reiniciarClic}
+        />
+      </div>
     </div>
   );
 }
